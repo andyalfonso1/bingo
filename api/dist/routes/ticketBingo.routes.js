ce33c57e6dbd874c480f2e3ee36e3dd64268abed
Andy <andyalfonso1@gmail.com>
2026-05-13 19:37:07 -0400

    init project

--- api/dist/routes/ticketBingo.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ticketBingoRoutes = ticketBingoRoutes;
-const ticketBingo_controller_1 = require("../controllers/ticketBingo.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-async function ticketBingoRoutes(fastify) {
-    fastify.get("/tickets-bingo/:number/bingos/:bingoId", { preHandler: auth_middleware_1.authMiddlewareAdmin }, ticketBingo_controller_1.ticketBingoController.getByNumberAndBingoId);
-}
